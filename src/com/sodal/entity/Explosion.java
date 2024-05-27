21aec55c670bdc407ddb5029ff2c97881c4217c7
Asha Sodal <ashasodal@MacServer.local>
2024-05-27 14:31:36 +0200

    started implementing on explosion animations

---
+++ src/com/sodal/entity/Explosion.java
@@ -0,0 +1,21 @@
+package com.sodal.entity;
+
+import java.awt.*;
+
+public class Explosion extends Entity {
+    public Explosion(String imagePath, double scale) {
+        super(imagePath, scale);
+    }
+
+    @Override
+    public void render(Graphics2D g2) {
+
+        g2.drawImage(this.getBufferedImage(), this.getX(), this.getY(), null);
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
